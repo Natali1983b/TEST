971e4c0007eaa6cc054d891eaa6c452c5f1676dd
Natali <nbyka@mail.ru>
2023-04-29 21:31:08 +0300

    Solution

---
+++ Solution/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.Write("Введите количество элементов массива: ");
+int m = int.Parse(Console.ReadLine());
+string [] stringArray = new string [m];
+void array(string [] stringArray)
+{
+  for (int i = 0;i<stringArray.Length;i++)
+  {
+     Console.WriteLine($"Введите {i+1} элемент массива");
+     stringArray[i] = Console.ReadLine();
+  }
+}
+string [] symbol(string [] stringArray)
+{
+  int count = 0;
+  for (int i = 0;i<stringArray.Length;i++)
+  {
+    if(stringArray[i].Length <=3)
+    count++;
+  }
+  string [] Array2 = new string [count];
+  int index = 0;
+  for (int i = 0;i<stringArray.Length;i++)
+  {
+    if(stringArray[i].Length <=3)
+    {
+        Array2[index] = stringArray[i];
+        index++;
+    }
+  }
+  return Array2;
+}
+void printA(string [] stringArray)
+{
+    Console.Write("[");
+    for (int i = 0;i<stringArray.Length;i++)
+    {
+    Console.Write($"‘{stringArray[i]}’, ");
+    }
+    Console.Write("]");
+}
+array(stringArray);
+printA(symbol(stringArray));
